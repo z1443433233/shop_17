396b4e38c932196459e206b6fec1896f0e234725
z1443433233 <1443433233@qq.com>
2019-12-17 21:31:17 +0800

    重置下

--- src/main/java/com/qfedu/index/dao/IndexMapper.java
+++
@@ -1,9 +0,0 @@
-package com.qfedu.index.dao;
-
-import com.qfedu.index.entity.Shop;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface IndexMapper {
-    Shop findShop();
-}
--- src/main/java/com/qfedu/index/service/IndexService.java
+++
@@ -1,7 +0,0 @@
-package com.qfedu.index.service;
-
-import com.qfedu.index.entity.Shop;
-
-public interface IndexService {
-    Shop findShop();
-}
